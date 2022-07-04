82b83d36e3f94c2652de0ec4572cea7dcf37df91
Stefan Pribicevic <stefan.pribicevic@ogury.co>
2022-07-04 20:30:49 +0200

    List component functionality.

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,14 +1,29 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import AddListItem from "../components/Item/AddListItem";
 import Item from "../components/Item/Item";
+import { removeListItem } from "../store/lists";
 
 const List = () => {
+  const dispatch = useDispatch();
   const { index } = useParams();
   const list = useSelector(state => state.lists.entities[index]);
+  const items = useSelector(state => state.items);
 
   return (
     <div>
-      {list.items.map(item => <Item name={item.name} />)}
+      <div>
+        {list.items.map(({ name, count, measure }) =>
+          <Item
+            key={name}
+            name={name}
+            count={count}
+            measure={measure}
+            removeItem={() => dispatch(removeListItem({ listIndex: index, name }))}
+          />
+        )}
+      </div>
+      <AddListItem items={items} />
     </div>
   );
 };
