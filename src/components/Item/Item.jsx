fcf496907a3d541863a0b43eafcf5a378b11ded8
Stefan Pribicevic <stefan.pribicevic@ogury.co>
2022-07-04 18:23:44 +0200

    Item component modified to support list item.

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,7 +1,9 @@
-const Item = ({ name, removeItem, onClick = () => { } }) => (
+const Item = ({ name, count = "", measure = "", removeItem, onClick = () => { } }) => (
   <div className="text-center font-nanum-brush-scipt text-[30px] relative">
     <span onClick={onClick}>
-      {name}
+      <span>{name}</span>
+      {count && <span>{` `}{count}</span>}
+      {measure && <span>{` `}{measure}</span>}
     </span>
     <span className="absolute right-[20px]" onClick={removeItem}>✕</span>
   </div>
